3da86350dc86534a62d7ab84e8ea57ea969e6178
Patric Eckhart <mail@patriceckhart.com>
2024-08-01 13:19:04 +0200

    initial commit

---
+++ Classes/Controller/AuthenticationController.php
@@ -0,0 +1,103 @@
+<?php
+namespace SteinbauerIT\MicrosoftEntraIdAuthentication\Controller;
+
+/*
+ * This file is part of the SteinbauerIT.MicrosoftEntraIdAuthentication package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Exception;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+use Neos\Flow\Mvc\View\JsonView;
+use Neos\Flow\Security\Context;
+use SteinbauerIT\AzureAuthPhpClient\GraphApiClient;
+use Microsoft\Kiota\Abstractions\ApiException;
+use Microsoft\Kiota\Authentication\Oauth\AuthorizationCodeContext;
+use Microsoft\Kiota\Authentication\PhpLeagueAuthenticationProvider;
+use Microsoft\Kiota\Http\GuzzleRequestAdapter;
+use SteinbauerIT\MicrosoftEntraIdAuthentication\Service\AuthenticationService;
+
+#[Flow\Scope("singleton")]
+final class AuthenticationController extends ActionController
+{
+
+    /**
+     * @var string
+     */
+    protected $defaultViewObjectName = JsonView::class;
+
+    #[Flow\InjectConfiguration(path: 'options.clientId', package: 'SteinbauerIT.MicrosoftEntraIdAuthentication')]
+    protected string $clientId;
+
+    #[Flow\InjectConfiguration(path: 'options.clientSecret', package: 'SteinbauerIT.MicrosoftEntraIdAuthentication')]
+    protected string $clientSecret;
+
+    #[Flow\InjectConfiguration(path: 'options.tenantId', package: 'SteinbauerIT.MicrosoftEntraIdAuthentication')]
+    protected string $tenantId;
+
+    #[Flow\InjectConfiguration(path: 'onAuthenticationSuccess.redirectToUri', package: 'SteinbauerIT.MicrosoftEntraIdAuthentication')]
+    protected string $redirectToUri;
+
+    #[Flow\Inject]
+    protected AuthenticationService $authenticationService;
+
+    #[Flow\Inject]
+    protected Context $securityContext;
+
+    /**
+     *
+     * @return void
+     */
+    public function loginAction(): void
+    {
+        $uri = 'https://login.microsoftonline.com/' . $this->tenantId . '/oauth2/v2.0/authorize?client_id=' . $this->clientId . '&response_type=code&redirect_uri=' . $this->getRedirectUri() . '&response_mode=query&scope=User.Read&state=12345';
+        $this->redirectToUri($uri);
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function authenticateAction(): void
+    {
+        if($this->request->hasArgument('code')) {
+            try {
+                $allowedHosts = ['graph.microsoft.com'];
+                $scopes = ['User.Read'];
+
+                $tokenRequestContext = new AuthorizationCodeContext(
+                    $this->tenantId,
+                    $this->clientId,
+                    $this->clientSecret,
+                    $this->request->getArgument('code'),
+                    $this->getRedirectUri()
+                );
+
+                $authProvider = new PhpLeagueAuthenticationProvider($tokenRequestContext, $scopes, $allowedHosts);
+                $requestAdapter = new GuzzleRequestAdapter($authProvider);
+                $client = new GraphApiClient($requestAdapter);
+
+                $me = $client->me()->get()->wait();
+
+                if($this->authenticationService->authenticate($me)) {
+                    $this->redirectToUri($this->redirectToUri);
+                }
+                $this->view->assign('value', ['response' => 'success', 'account' => $this->securityContext->getAccount()->getAccountIdentifier()]);
+            } catch (ApiException $ex) {
+                throw new Exception($ex->getMessage(), $ex->getCode());
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function getRedirectUri(): string
+    {
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $domainName = $_SERVER['HTTP_HOST'];
+        return $protocol . $domainName . '/mseid/auth';
+    }
+
+}
---
+++ Classes/Service/AuthenticationService.php
@@ -0,0 +1,105 @@
+<?php
+namespace SteinbauerIT\MicrosoftEntraIdAuthentication\Service;
+
+/*
+ * This file is part of the SteinbauerIT.MicrosoftEntraIdAuthentication package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Security\Account;
+use Neos\Flow\Security\AccountRepository;
+use Neos\Flow\Security\Authentication\AuthenticationManagerInterface;
+use Neos\Flow\Security\Authentication\Token\UsernamePassword;
+use Neos\Flow\Security\Authentication\TokenInterface;
+use Neos\Flow\Security\Context;
+use Neos\Flow\Security\Policy\PolicyService;
+use Psr\Log\LoggerInterface;
+use Neos\Flow\Log\Utility\LogEnvironment;
+use SteinbauerIT\AzureAuthPhpClient\Models\User;
+
+class AuthenticationService
+{
+
+    #[Flow\InjectConfiguration(path: 'account.accountIdentifierProperty', package: 'SteinbauerIT.MicrosoftEntraIdAuthentication')]
+    protected string $accountIdentifierProperty;
+
+    #[Flow\InjectConfiguration(path: 'account.authenticationProviderName', package: 'SteinbauerIT.MicrosoftEntraIdAuthentication')]
+    protected string $authenticationProviderName;
+
+    #[Flow\InjectConfiguration(path: 'account.allowedRoles', package: 'SteinbauerIT.MicrosoftEntraIdAuthentication')]
+    protected array $allowedRoles;
+
+    #[Flow\Inject]
+    protected AccountRepository $accountRepository;
+
+    #[Flow\Inject]
+    protected AuthenticationManagerInterface $authenticationManager;
+
+    #[Flow\Inject]
+    protected PolicyService $policyService;
+
+    #[Flow\Inject]
+    protected Context $securityContext;
+
+    /**
+     * @Flow\Inject(name="Neos.Flow:SecurityLogger")
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @param User $user
+     * @return bool
+     * @throws \Exception
+     */
+    public function authenticate(User $user): bool
+    {
+        if(array_key_exists($this->accountIdentifierProperty, $user->getAdditionalData())) {
+            $accountIdentifier = $user->getAdditionalData()[$this->accountIdentifierProperty];
+        } else {
+            $accountIdentifier = $user[$this->accountIdentifierProperty];
+        }
+
+        $this->logger->debug(sprintf('Microsoft Entra ID Authentication: Starting authentication for user "%s" ...', $accountIdentifier), LogEnvironment::fromMethodName(__METHOD__));
+
+        $account = $this->accountRepository->findByAccountIdentifierAndAuthenticationProviderName($accountIdentifier, $this->authenticationProviderName);
+
+        if($this->rolesAreAllowed($account)) {
+
+            $account->authenticationAttempted(TokenInterface::AUTHENTICATION_SUCCESSFUL);
+
+            $tokens = $this->securityContext->getAuthenticationTokensOfType(UsernamePassword::class);
+
+            /* @var UsernamePassword $token */
+            foreach ($tokens as $token) {
+                $token->setAccount($account);
+                $token->setAuthenticationStatus(TokenInterface::AUTHENTICATION_SUCCESSFUL);
+            }
+            $this->authenticationManager->authenticate();
+
+            $this->logger->debug(sprintf('Microsoft Entra ID Authentication: Successfully authenticated account "%s" with authentication provider %s.', $account->getAccountIdentifier(), $account->getAuthenticationProviderName()), LogEnvironment::fromMethodName(__METHOD__));
+
+            return true;
+
+        }
+
+        $this->logger->debug(sprintf('Microsoft Entra ID Authentication: The roles of account %s are not in the allowedRoles in the Configuration.', $account->getAccountIdentifier()), LogEnvironment::fromMethodName(__METHOD__));
+        return false;
+    }
+
+    /**
+     * @param Account $account
+     * @return bool
+     */
+    public function rolesAreAllowed(Account $account): bool
+    {
+        $roles = $account->getRoles();
+        foreach ($roles as $role) {
+            if(in_array($role->getIdentifier(), $this->allowedRoles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
